fe833776fb351123a605f0fe4056958e77c3f5fc
m93n <mohsen.k9731@gmail.com>
2023-09-15 17:32:47 +0330

    init auth project and customuser model in accounts app and test

---
+++ accounts/tests.py
@@ -0,0 +1,28 @@
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+
+
+class CustomUserTests(TestCase):
+    def test_create_user(self):
+        User = get_user_model()
+        user = User.objects.create_user(
+            email='testuser@email.com', username='testuser', password='testpass123'
+        )
+        
+        self.assertEqual(user.username, 'testuser')
+        self.assertEqual(user.email, 'testuser@email.com')
+        self.assertTrue(user.is_active)
+        self.assertFalse(user.is_staff)
+        self.assertFalse(user.is_superuser)
+    
+    def test_create_superuser(self):
+        User = get_user_model()
+        user = User.objects.create_superuser(
+            email='testsuperadmin@email.com', username='testsuperadmin', password='testpass123'
+        )
+        
+        self.assertEqual(user.username, 'testsuperadmin')
+        self.assertEqual(user.email, 'testsuperadmin@email.com')
+        self.assertTrue(user.is_active)
+        self.assertTrue(user.is_staff)
+        self.assertTrue(user.is_superuser)
